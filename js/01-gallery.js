afe2dc9e99b0db9ba54369090b509e5d238fc811
Anna Vasylchenko <anna.vasilchenko.2017@gmail.com>
2023-04-09 23:30:49 +0300

    12

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const itemUl = document.querySelector('.gallery');
+
+
+
+const marupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="galerry__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src=
+"${preview}" data-source="${original}" alt="${description}"></a></li>`;
+}).join('');
+
+itemUl.insertAdjacentHTML('beforeend', marupGallery)
+
+itemUl.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) { 
+    event.preventDefault();
+   
+    const currentItem = event.target;
+    
+
+    if (currentItem.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${currentItem.dataset.source}" width="800" height="600">
+`)
+    
+    instance.show()
+    window.addEventListener('keydown', onCloseModalClick);
+
+}
+
+
+
+function onCloseModalClick(event) {
+    const openInstance = document.querySelector(".basicLightbox")
+    if (event.code === "Escape") {
+        openInstance.remove();
+        // console.log(openInstance);
+    }
+    window.removeEventListener('keydown', onCloseModalClick);
+   
+   
+}
+
+console.log(galleryItems);
